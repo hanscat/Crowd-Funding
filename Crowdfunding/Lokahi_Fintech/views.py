b701251066557b11af1006c7be8290ce497e11de
Will Cricchi <wcc4ch@virginia.edu>
2017-03-26 16:25:18 -0400

    Removed report from installed apps

--- Crowdfunding/Lokahi_Fintech/views.py
+++ Crowdfunding/Lokahi_Fintech/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+import models.py
+
 import urllib.request, json
 
 # Create your views here.

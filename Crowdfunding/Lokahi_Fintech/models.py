2deed288c75b693162b2ebc05662792cbc0ec8e4
Jake Mathews <jwm5vv@virginia.edu>
2017-04-10 17:06:59 -0400

    Added group model and slightly tweaked user and report models

--- Crowdfunding/Lokahi_Fintech/models.py
+++ Crowdfunding/Lokahi_Fintech/models.py
@@ -38,11 +38,14 @@ Can only be deleted by SM user
     sector = models.CharField(max_length=60)
     Industry = models.CharField(max_length=60)
     current_projects = models.TextField()
-
+    title = models.CharField(max_length=100, default=str(Company_name) + " New Report")
     file = models.FileField(blank = True)
     encrypted = models.BooleanField(blank = True)
 
     private = models.BooleanField()
+
+    def __str__(self):
+        return self.title
 
 
 class user(models.Model):
@@ -51,13 +54,30 @@ class user(models.Model):
     last_name  = models.CharField(max_length=20, default="user")
     user_name = models.CharField(max_length=30, unique = True, default="newUser")
     password = models.CharField(max_length=30, default="password")
+
     class Meta:
         managed = True
 
+    def __str__(self):
+        return self.user_name
 
+import json
+from django.contrib.postgres.fields import JSONField
 
+class group(models.Model):
+    name = models.CharField(max_length=100, default="new group")
+    users = JSONField()
+    reports = JSONField()
 
-def __str__(self):
-    return self.title
+    #we can decide later whether we want to give groups access to reports here or include
+    #which groups get access within the report model, i don't think it makes a big difference. Maybe both.
+    #The same principle applies to which users and groups.
+
+    #using the JSONField allows data to be stored in the users field, mainly as a dictionary,
+    #so we will have usernames and actual names, as well as possibly permissions all stored
+    #in this JSONField. I looked around, and thats the best way to do it.
+
+    def __str__(self):
+        return self.name
 
 

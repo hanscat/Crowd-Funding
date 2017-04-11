d586d82472b97c7d4bb2d37ec0bc87f7afb00de0
Hanscat <hanscat@icloud.com>
2017-04-11 18:13:40 -0400

    deleted user model, using built-in user model instead

--- Crowdfunding/Lokahi_Fintech/forms.py
+++ Crowdfunding/Lokahi_Fintech/forms.py
@@ -1,14 +1,18 @@
 from django import forms
+from django.core import validators
 
 class UserForm(forms.Form):
     #to take the input of username
     username = forms.CharField(max_length=100, required=True)
     # to take the input of email
-    email = forms.CharField(max_length=100, required=True)
+    email = forms.EmailField(help_text='A valid email address, please.')
     # to take the input of password
     password = forms.CharField(max_length=100, required=True, widget=forms.PasswordInput())
     retype_password = forms.CharField(max_length=100, required=True, widget=forms.PasswordInput())
     
-class Login(forms.Form):
+class loginFrom(forms.Form):
     username = forms.CharField(max_length=100, required=True)
     password = forms.CharField(max_length=100, required=True, widget=forms.PasswordInput())
+    
+    
+    
--- Crowdfunding/Lokahi_Fintech/views.py
+++ Crowdfunding/Lokahi_Fintech/views.py
@@ -1,19 +1,38 @@
 from django.shortcuts import render
-from . forms import UserForm
-from . models import User
+from .forms import UserForm
+from .models import User
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout, authenticate, login
 from django.http import HttpResponse
 from django.template import loader
 import urllib.request, json
 import operator
 from django.db.models import Q
 
+
 # Create your views here.
 def index(request):
+    if not request.user.is_authenticated:
+        return render(request, 'home.html')
     all_users = User.objects.all()
     return render(request, 'home.html', )
 
-def login(request):
+
+def login_page(request):
     return render(request, 'login.html')
+
+def my_login(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        login(request, user)
+        # Redirect to a success page.
+        return render(request, 'home.html')
+    else:
+        # Return an 'invalid login' error message.
+        return render(request, 'login.html')
+
 
 def signup(request):
     if request.method == 'POST':  # if the form has been filled
@@ -23,23 +42,28 @@ def signup(request):
         if form.is_valid():  # All the data is valid
             username = request.POST.get('username', '')
             email = request.POST.get('email', '')
-        password = request.POST.get('password', '')
-        # creating an user object containing all the data
-        user_obj = User(username=username, email=email, password=password)
-        # saving all the data in the current object into the database
-        user_obj.save()
-
-        return render(request, 'signup.html', {'user_obj': user_obj,'is_registered':True }) # Redirect after POST
-
+            password = request.POST.get('password', '')
+            # creating an user object containing all the data
+            user_obj = User(username=username, email=email, password=password)
+            # saving all the data in the current object into the database
+            user_obj.save()
+            return render(request, 'signup.html', {'user_obj': user_obj, 'is_registered': True})  # Redirect after POST
     else:
         form = UserForm()  # an unboundform
 
         return render(request, 'signup.html', {'form': form})
-    
-#the function executes with the showdata url to display the list of registered users
+
+
+# the function executes with the showdata url to display the list of registered users
 def showUsers(request):
     all_users = User.objects.all()
     return render(request, 'userdetail.html', {'all_users': all_users, })
 
+
+@login_required
 def logout(request):
+    # do something to log out
+    logout(request)
     return (request, 'logout.html')
+
+

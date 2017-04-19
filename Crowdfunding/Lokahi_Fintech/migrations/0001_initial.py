a0ea37747d9f9e8e1fbaca2ffcd753ffc8c3c6e2
Will Cricchi <wcc4ch@virginia.edu>
2017-04-19 16:58:20 -0400

    My working code

--- Crowdfunding/Lokahi_Fintech/migrations/0001_initial.py
+++ Crowdfunding/Lokahi_Fintech/migrations/0001_initial.py
@@ -2,38 +2,82 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-import django.utils.timezone
+import datetime
+from django.conf import settings
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Report',
+            name='Document',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('timestamp', models.DateTimeField(default=django.utils.timezone.now)),
-                ('Company_name', models.CharField(max_length=60)),
-                ('Company_phone', models.CharField(max_length=12)),
-                ('Company_Location', models.CharField(max_length=60)),
-                ('sector', models.CharField(max_length=60)),
-                ('Industry', models.CharField(max_length=60)),
-                ('current_projects', models.TextField()),
-                ('file', models.FileField(upload_to='', blank=True)),
-                ('encrypted', models.BooleanField()),
-                ('private', models.BooleanField()),
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
+                ('name', models.CharField(max_length=100, blank=True, null=True, default=None)),
+                ('encrypted', models.BooleanField(default=False)),
+                ('file', models.FileField(upload_to='documents')),
             ],
         ),
         migrations.CreateModel(
-            name='User',
+            name='File',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('username', models.CharField(max_length=100)),
-                ('email', models.CharField(max_length=100)),
-                ('password', models.CharField(max_length=100)),
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
+                ('name', models.CharField(max_length=100, blank=True, null=True, default=None)),
+                ('encrypted', models.BooleanField(default=False)),
+                ('filename', models.FileField(upload_to='documents')),
             ],
+        ),
+        migrations.CreateModel(
+            name='Group',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
+                ('title', models.CharField(max_length=100)),
+                ('owner', models.CharField(max_length=100, default='')),
+                ('participants', models.ManyToManyField(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Investor',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
+                ('name', models.CharField(max_length=100)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Messages',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
+                ('msg_content', models.CharField(max_length=1000)),
+                ('receiver', models.ForeignKey(related_name='receiver', to=settings.AUTH_USER_MODEL)),
+                ('sender', models.ForeignKey(related_name='sender', to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Profile',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
+                ('role', models.CharField(max_length=20, choices=[('C', 'Company'), ('I', 'Investor'), ('N/A', 'not declared')])),
+                ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Report',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
+                ('date', models.CharField(max_length=200, default=datetime.date.today)),
+                ('title', models.CharField(max_length=100, default='')),
+                ('Company', models.CharField(max_length=60)),
+                ('Industry', models.CharField(max_length=60)),
+                ('private', models.BooleanField()),
+            ],
+        ),
+        migrations.AddField(
+            model_name='file',
+            name='report',
+            field=models.ForeignKey(null=True, to='Lokahi_Fintech.Report'),
         ),
     ]

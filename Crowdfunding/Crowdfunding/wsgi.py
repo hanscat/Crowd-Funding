602e02e0f401212777fcbc934fcafa69c584ed3d
Hanscat <hanscat@icloud.com>
2017-05-02 19:15:09 -0400

    add haystack to requirements.txt

--- Crowdfunding/Crowdfunding/wsgi.py
+++ Crowdfunding/Crowdfunding/wsgi.py
@@ -11,6 +11,13 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Crowdfunding.settings")
+# 
+# application = get_wsgi_application()
+import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Crowdfunding.settings")
 
-application = get_wsgi_application()
+# This application object is used by the development server
+# as well as any WSGI server configured to use this file.
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()

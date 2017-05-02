3183f68c38dc4bc6a00c21b73d35d1167a1f2939
Phillip Seaton <pbs5kx@virginia.edu>
2017-05-02 18:17:20 -0400

    sitemanage

--- Crowdfunding/Lokahi_Fintech/search_indexes.py
+++ Crowdfunding/Lokahi_Fintech/search_indexes.py
@@ -6,11 +6,18 @@ class ReportIndex(SearchIndex, Indexable):
     owner = CharField(model_attr='owner')
     title = CharField(model_attr='title')
     company = CharField(model_attr='company')
+    ceo = CharField(model_attr='ceo')
     sector = CharField(model_attr='sector')
     industry = CharField(model_attr='industry')
     location = CharField(model_attr='location')
     country = CharField(model_attr='country')
     projects = CharField(model_attr='projects')
+    projects2 = CharField(model_attr='projects2')
+    projects3 = CharField(model_attr='projects3')
+    projects4 = CharField(model_attr='projects4')
+    projects5 = CharField(model_attr='projects5')
+
+
 
     def get_model(self):
         return Report

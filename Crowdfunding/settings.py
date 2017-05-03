95e4537ea8c6ef041db104b45e499a38f9e2d954
Hanscat <hanscat@icloud.com>
2017-05-02 20:49:30 -0400

    whitenoise added

--- Crowdfunding/settings.py
+++ Crowdfunding/settings.py
@@ -137,3 +137,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, "／static/")
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
--- Crowdfunding/wsgi.py
+++ Crowdfunding/wsgi.py
@@ -22,4 +22,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Crowdfunding.settings")
 # import django.core.handlers.wsgi
 # application = django.core.handlers.wsgi.WSGIHandler()
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
